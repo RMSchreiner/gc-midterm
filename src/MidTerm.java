90f729f996c97c41e788b46fb791da72b54a31a7
Tom Rowland <thomasjrowland@gmail.com>
2020-05-07 10:24:21 -0400

    more changes

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -6,7 +6,8 @@ public class MidTerm {
 		System.out.println("Once upon a time in the mystical land of "
 				+ "Shangrila there dwelt an immortal named Kai-Yun and "
 				+ "his trusty band of ninja monkeys.");
-		System.out.println("Udikan the Warrior was determined to prove that Kai-Yun is no mortal.");
+		System.out.println("Udikan the Warrior was determined to prove that Kai-Yun is no immortal.");
+		System.out.println("Kai-Yun was not afraid, for he had his enchanted coffee pot at his side.");
 		
 
 	}
