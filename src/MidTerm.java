640e9453ece43eb2e0e12fabee61bff8cf115498
Tom Rowland <thomasjrowland@gmail.com>
2020-05-07 11:15:11 -0400

    kai to david

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -4,13 +4,13 @@ public class MidTerm {
 	public static void main(String[] args) {
 
 		System.out.println("Once upon a time in the mystical land of "
-				+ "Shangrila there dwelt an immortal named Kai-Yun and "
+				+ "Shangrila there dwelt an immortal named David and "
 				+ "his trusty band of ninja monkeys.");
-		System.out.println("Udikan the Warrior was determined to prove that Kai-Yun is no immortal.");
-		System.out.println("Kai-Yun was not afraid, for he had his enchanted coffee pot at his side.");
+		System.out.println("Udikan the Warrior was determined to prove that David is no immortal.");
+		System.out.println("David was not afraid, for he had his enchanted coffee pot at his side.");
 		System.out.println("Udikan did not know that his motherbore him after drinking coffee from this "
 				+ "enchanted coffee pot");
-		System.out.println("This meant that only Kai-Yun could slay him.");
+		System.out.println("This meant that only David could slay him.");
 	}
 
 }
