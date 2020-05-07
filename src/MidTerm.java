121f7d892f1fda2f4bbbc4f46d7ca34d7fd78b9d
Randolph Schreiner <Rschreiner83@gmail.com>
2020-05-07 11:14:22 -0400

    conflict

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -4,7 +4,7 @@ public class MidTerm {
 	public static void main(String[] args) {
 
 		System.out.println("Once upon a time in the mystical land of "
-				+ "Shangrila there dwelt an immortal named Kai-Yun and "
+				+ "Detroit there dwelt an immortal named Kai-Yun and "
 				+ "his trusty band of ninja monkeys.");
 		System.out.println("Udikan the Warrior was determined to prove that Kai-Yun is no immortal.");
 		System.out.println("Kai-Yun was not afraid, for he had his enchanted coffee pot at his side.");
